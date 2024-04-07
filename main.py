4d9d18a5efd6df519d3fae7f81d1d6dd8f78ebf1
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-07 14:03:26 -0400

    setup models from the docs

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    return {"message": "Its working"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
